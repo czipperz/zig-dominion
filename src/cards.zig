86febef0dade88611084cc00fe2bcb74dea53974
Chris Gregory <czipperz@gmail.com>
2021-07-31 21:29:13 -0700

    Add curse card

--- src/cards.zig
+++ src/cards.zig
@@ -1,3 +1,5 @@
 usingnamespace @import("cards/copper.zig");
 usingnamespace @import("cards/silver.zig");
 usingnamespace @import("cards/gold.zig");
+usingnamespace @import("cards/curse.zig");
+usingnamespace @import("cards/estate.zig");
---
+++ src/cards/curse.zig
@@ -0,0 +1,22 @@
+usingnamespace @import("../card.zig");
+usingnamespace @import("../victory.zig");
+
+pub const curse = CardClass {
+    .name = "Curse",
+    .cost = 0,
+    .description = "Worth -1 victory point",
+    .type = .curse,
+    .action = doNothing,
+    .victory_points = staticScore(-1),
+};
+
+usingnamespace @import("../scenario.zig");
+
+test {
+    var scenario = try Scenario.simple(2);
+    defer scenario.deinit();
+
+    try expect(scenario.state.activePlayer().victoryPoints(&scenario.state) == 3);
+    try scenario.state.activePlayer().deck.append(&curse);
+    try expect(scenario.state.activePlayer().victoryPoints(&scenario.state) == 2);
+}
